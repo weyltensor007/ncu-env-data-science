831bee5dca64274525dbf4004b3463547650eb56
weyltensor007 <weyltensor@gmail.com>
2026-05-07 11:48:51 +0800

    converting script for md to pdf

---
+++ Assignment_3/convert_md.bat
@@ -0,0 +1,10 @@
+@echo off
+
+pandoc assignment_3.md -o assignment_3.pdf ^
+--pdf-engine=xelatex ^
+-V mainfont="Microsoft JhengHei" ^
+--resource-path=.:imgs ^
+-V colorlinks=true ^
+-V linkcolor=blue ^
+-V urlcolor=cyan ^
+-V citecolor=magenta ^ && start assignment_3.pdf
